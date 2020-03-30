55b9c84c7ffa2c6dd796f9dd54dec0e3d29aa288
Unrimah <unrimah@gmail.com>
2020-03-30 23:21:29 +0300

    New rabbits

--- Node2D.gd
+++ Node2D.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const NUM_GRASS = 10
+const NUM_RABBITS = 3
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -7,13 +9,31 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	_position_rab_and_grow()
+	var grass_scene = preload("res://GrassBody2D.tscn")
+	var rabbit_scene = preload("res://RabbitBody2D.tscn")
+
+	for i in NUM_GRASS:
+		var grass_node = grass_scene.instance()
+		grass_node.position.x = randi() % 1200
+		grass_node.position.y = randi() % 600
+		grass_node.add_to_group("grass")
+		add_child(grass_node)
+	
+	for i in NUM_RABBITS:
+		var rabbit_node = rabbit_scene.instance()
+		rabbit_node.position.x = randi() % 1200
+		rabbit_node.position.y = randi() % 600
+		rabbit_node.add_to_group("rabbits")
+		add_child(rabbit_node)
+	
+#	_position_rab_and_grow()
+	
 	
 func _position_rab_and_grow():
 #	var posX = randi() % 1366
 #	var posY = randi() % 768
-	get_node("KrolikVashyMat").set_place(randi() % 1366, randi() % 768)
-	get_node("TRAVAblin").set_place(randi() % 1366, randi() % 768)
+	get_node("KrolikVashyMat").set_place(randi() % 1200, randi() % 600)
+	get_node("TRAVAblin").set_place(randi() % 1200, randi() % 600)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
---
+++ RabbitBody2D.gd
@@ -0,0 +1,67 @@
+extends RigidBody2D
+
+const MAX_HEALTH = 100
+const SEARCH_RANGE = 500
+const EATING_DISTANCE = 5
+
+enum ACT {
+	DIE,
+	MOVE,
+	EAT
+}
+
+var health
+onready var TARGET = get_node("../TRAVAblin").position
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.health = MAX_HEALTH
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	match select_action():
+		ACT.MOVE:
+			rabbit_move()
+		ACT.EAT:
+			pass
+		ACT.DIE:
+			rabbit_die()		
+
+func select_action():
+	return ACT.MOVE
+#	return ACT.DIE
+	
+func rabbit_move():
+	var grass_list = get_tree().get_nodes_in_group("grass")
+	var distance = SEARCH_RANGE
+	var direction
+	for grass in grass_list:
+		var grass_distance = self.position.distance_to(grass.position)
+		if distance > grass_distance:
+			distance = grass_distance
+			TARGET = grass
+	if distance == SEARCH_RANGE:
+		direction = Vector2(2*(randi() % 2)-1,2*(randi() % 2)-1)
+	else:
+		direction = Vector2(0,0)
+		if (self.position.x < TARGET.position.x) and (self.position.x < 1200):
+			direction.x = 1
+		if (self.position.y < TARGET.position.y) and (self.position.y < 600):
+			direction.y = 1
+		if (self.position.x > TARGET.position.x) and (self.position.x >= 0):
+			direction.x = -1
+		if (self.position.y > TARGET.position.y) and (self.position.y >= 0):
+			direction.y = -1
+	if distance < EATING_DISTANCE:
+		rabbit_eat(TARGET)
+	else:
+		global_translate(direction)
+
+func rabbit_eat(food):
+	food.free()
+	
+func set_place(x, y):
+	self.position = Vector2(x, y)
+	
+func rabbit_die():
+	pass
