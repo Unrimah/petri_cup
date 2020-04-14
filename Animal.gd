d8f92b483f05c474fedf6327fb8053d598607986
Unrimah <unrimah@gmail.com>
2020-04-14 23:14:44 +0300

    Prepared for loop_closest_position()

--- Animal.gd
+++ Animal.gd
@@ -83,18 +83,45 @@ func select_action(delta):
 	if health <  1:
 		return ACT.DIE
 	return ACT.TO_EAT
+
+func loop_distance_to(trg):
+	pass
+
+func loop_direction_to(trg):
+	pass
 	
+func loop_closest_position(trg_pos):
+	return trg_pos
+
+func normalize_position():
+	pass
 	
+func normalize_X(value):
+	while (value < 0):
+		value += X_RES
+	while (value > X_RES):
+		value -= X_RES
+	return value
+
+func normalize_Y(value):
+	while (value < 0):
+		value += Y_RES
+	while (value > Y_RES):
+		value -= Y_RES
+	return value
+
 func animal_to_eat(delta):
 	var food_list = get_tree().get_nodes_in_group(FOOD_GROUP)#Groups of food
 	var distance = SEARCH_RANGE
 	var direction
 	var TARGET
+	var trg_loop_pos
 	
 	health -= CONSUMPTION_RUN * delta
 	
 	for food in food_list:
-		var food_distance = self.position.distance_to(food.position)
+		trg_loop_pos = loop_closest_position(food.position)
+		var food_distance = self.position.distance_to(trg_loop_pos)
 		if distance > food_distance:
 			distance = food_distance
 			TARGET = food
@@ -104,15 +131,18 @@ func animal_to_eat(delta):
 	if distance < EATING_DISTANCE:
 		animal_eat(TARGET)
 		return
-	direction = Vector2((TARGET.position.x - position.x)/distance,(TARGET.position.y - position.y)/distance)
+	trg_loop_pos = loop_closest_position(TARGET.position)
+	direction = Vector2((trg_loop_pos.x - position.x)/distance,(trg_loop_pos.y - position.y)/distance)
 	direction *= SPEED_RUN * delta
 	global_translate(direction)
+	normalize_position()
 
 func animal_to_fuck(delta):
 	var animals_list = get_tree().get_nodes_in_group(SELF_GROUP)
 	var distance = SEARCH_RANGE
 	var direction
 	var TARGET
+	var trg_loop_pos
 
 	health -= CONSUMPTION_RUN * delta
 
@@ -120,7 +150,8 @@ func animal_to_fuck(delta):
 	for animal in animals_list:
 		if animal.get_instance_id() == self.get_instance_id():
 			continue
-		var animal_distance = self.position.distance_to(animal.position)
+		trg_loop_pos = loop_closest_position(animal.position)
+		var animal_distance = self.position.distance_to(trg_loop_pos)
 		if animal_distance < SEARCH_RANGE:
 			animals_in_range += 1
 		if animal.copulation_timer > 0:
@@ -134,9 +165,11 @@ func animal_to_fuck(delta):
 	if (animals_in_range < POPULATION_LIMIT) and (distance < COPULATING_DISTANCE):
 		animal_copulate(TARGET)
 		return
-	direction = Vector2((TARGET.position.x - position.x)/distance,(TARGET.position.y - position.y)/distance)
+	trg_loop_pos = loop_closest_position(TARGET.position)
+	direction = Vector2((trg_loop_pos.x - position.x)/distance,(trg_loop_pos.y - position.y)/distance)
 	direction *= SPEED_RUN * delta
 	global_translate(direction)
+	normalize_position()
 
 func animal_walk(delta):
 	health -= CONSUMPTION_WALK * delta
@@ -145,17 +178,19 @@ func animal_walk(delta):
 	if (walk_position == Vector2(0, 0)):
 		var walk_direction = randf() * 2 * PI
 		var walk_distance = randi() % (SEARCH_RANGE / 2) + 1 # To prevent X/0
-		var X = walk_distance * sin(walk_direction) + self.position.x
-		if X < 0: X = 0
-		if X > X_RES: X = X_RES
-		var Y = walk_distance * cos(walk_direction) + self.position.y
-		if Y < 0: Y = 0
-		if Y > Y_RES: Y = Y_RES
+		var X = normalize_X(walk_distance * sin(walk_direction) + self.position.x)
+#		if X < 0: X = 0
+#		if X > X_RES: X = X_RES
+		var Y = normalize_Y(walk_distance * cos(walk_direction) + self.position.y)
+#		if Y < 0: Y = 0
+#		if Y > Y_RES: Y = Y_RES
 		walk_position = Vector2(X, Y)
+		walk_position = loop_closest_position(walk_position)
 	var walk_distance = sqrt(pow((walk_position.x - position.x), 2) + pow((walk_position.y - position.y), 2))
 	var transition = Vector2((walk_position.x - position.x)/walk_distance,(walk_position.y - position.y)/walk_distance)
 	transition *= SPEED_WALK * delta
 	global_translate(transition)
+	normalize_position()
 
 func animal_eat(food):
 	health += food.BASE_HEALTH + food.health
